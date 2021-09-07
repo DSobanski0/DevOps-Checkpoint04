1d66388838953c48fe4a829c05f026e23263b6f8
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2021-09-07 18:29:35 -0300

    delete and update

--- src/main/java/br/com/fiap/banco/controller/ContaController.java
+++ src/main/java/br/com/fiap/banco/controller/ContaController.java
@@ -6,8 +6,10 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -42,7 +44,7 @@ public class ContaController {
 	}
 	
 	@PostMapping
-	public ModelAndView salvar (@Valid Conta conta, BindingResult result) {
+	public ModelAndView salvar(@Valid Conta conta, BindingResult result) {
 		if (result.hasErrors()) return new ModelAndView("conta-form");
 		ModelAndView mv = new ModelAndView("home");
 		contaRepository.save(conta);
@@ -50,5 +52,28 @@ public class ContaController {
 		mv.addObject("cliente", clientes.get(0));
 		mv.addObject("contas", contaRepository.findAll());
 		return mv;
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String editConta(@PathVariable("id") Long id, Model model) {
+		Conta conta = contaRepository.getById(id);
+		model.addAttribute(conta);
+		
+		return "editConta";
+	}
+	
+	@PostMapping("/update/{id}")
+	public String updateConta(@PathVariable("id") Long id, @Valid Conta conta, BindingResult result, Model model) {
+		if(result.hasErrors()) return "home";
+		
+		conta.setId(id);
+		contaRepository.save(conta);
+		return "redirect:/conta";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteConta(@PathVariable("id") Long id) {
+		contaRepository.deleteById(id);
+		return "redirect:/conta";
 	}
 }
